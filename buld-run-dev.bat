dca3730bd27c5f43ede1d4878e6990da05f46911
Anton Cherginets <anton.cherginets@gmail.com>
2022-05-05 23:11:41 +0300

    some fixes

--- buld-run-dev.bat
+++ buld-run-dev.bat
@@ -1,6 +1,6 @@
 REM building images
 docker-compose --env-file ./.env-dev -f docker-compose.yml -f docker-compose.dev.yml build &
 REM installing backend
-docker-compose --env-file ./.env-dev run php_fpm composer install &
+REM docker-compose --env-file ./.env-dev run php_fpm composer install --prefer-dist &
 REM running
 docker-compose --env-file ./.env-dev -f docker-compose.yml -f docker-compose.dev.yml up
--- buld-run-prod.bat
+++ buld-run-prod.bat
@@ -1,8 +1,8 @@
 REM building images
 docker-compose --env-file ./.env-prod -f docker-compose.yml -f docker-compose.prod.yml build &
 REM installing backend
-docker-compose --env-file ./.env-prod -f docker-compose.yml -f docker-compose.prod.yml run php_fpm composer install &
+REM docker-compose --env-file ./.env-prod -f docker-compose.yml -f docker-compose.prod.yml run php_fpm composer install &
 REM installing node-js modules
-docker-compose --env-file ./.env-prod -f docker-compose.yml -f docker-compose.prod.yml run nextjs_front bash -c "npm install && npm run build" &
+docker-compose --env-file ./.env-prod -f docker-compose.yml -f docker-compose.prod.yml run nextjs_front bash -c "yarn install && yarn run build" &
 REM running
 docker-compose --env-file ./.env-prod -f docker-compose.yml -f docker-compose.prod.yml up
